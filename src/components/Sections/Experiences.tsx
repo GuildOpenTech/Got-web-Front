a95a56b261f0be8d32f616b0b046ef2caf6d309c
Sarra1985 <110898069+Sarra1985@users.noreply.github.com>
2024-07-18 10:04:01 +0200

    Update Experiences.tsx

--- src/components/Sections/Experiences.tsx
+++ src/components/Sections/Experiences.tsx
@@ -1,13 +1,54 @@
-import Section from '../Section'
-
+import { Box } from "@mui/material"
+import Section from "../../components/Section"
+import CTAButton from "../../core/ui/CTAButton"
+import ExperienceCarousel from "../Experiences/ExperienceCarousel"
+import ExperienceButton from "../Experiences/ExperienceButton"
+//import Section from "../Section"
 export default function Experiences() {
   return (
-    <Section
-      title="Ils racontent leur expérience"
-      id="feedbacks"
-    >
-      <p>content of section here</p>
-    </Section>
+  <Box
+     paddingLeft="75px"
+     paddingTop='72px'
+     paddingBottom="37px"
+     >
+    
+  <Box
+     >
+       <Section  title="Ils racontent leur expérience"
+        id="feedbacks" 
+        scrollBarDist='28px'
+       >
+          <p></p>
 
-  )
-}
+        <Box
+        textAlign='center'
+        width='100%'
+        sx={{
+          marginTop: '24px',
+          
+        }}
+      >
+
+        <ExperienceCarousel/>
+        <div style={{
+          paddingBottom: "37px"
+        }}></div>
+        <ExperienceButton
+          href="/inscription"
+          style={{
+            width: '279px',
+            height: '73px',
+            fontSize: '30px',
+            fontWeight: '400',
+          }}
+          
+        >
+          Je suis motivé(e)
+        </ExperienceButton>
+
+      </Box>
+
+      </Section>
+      </Box>
+      </Box>
+      )}
